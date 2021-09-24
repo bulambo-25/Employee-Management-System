ecc06ee9b5beb1122526b38808e9492e03886721
ColeColombia <benzemabenelzane@yahoo.com>
2021-09-24 23:24:41 +0200

    removed unnecessary file paths

--- admin/private/initialize.php
+++ admin/private/initialize.php
@@ -7,11 +7,7 @@ define("APP_ROOT", dirname(dirname(__FILE__)));
 define("PRIVATE_PATH", APP_ROOT . "/private");
 define("PUBLIC_PATH", APP_ROOT . "/public");
 
-require_once(PRIVATE_PATH . "/database.php");
 require_once(PRIVATE_PATH . "/crud.php");
-require_once(PRIVATE_PATH . "/userRegistration.php");
-require_once(PRIVATE_PATH . "/admin.php");
-require_once(PRIVATE_PATH . "/passwords.php");
 require_once(PRIVATE_PATH . "/functions.php");
 
  ?>
